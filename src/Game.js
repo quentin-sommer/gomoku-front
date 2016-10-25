b03b0397fe8592ee1e760d8cb605b0a6c78e1c40
Quentin Sommer <sommerquentin67@gmail.com>
2016-10-25 14:50:42 +0200

    more refactor, extracted board size to config file

--- src/Game.js
+++ src/Game.js
@@ -13,28 +13,9 @@ import {
     ArcRotateCamera,
     HemisphericLight
 } from 'babylonjs';
+import config from './config'
 
-class Game extends React.Component {
-  componentDidMount() {
-    babylonInit();
-  }
-
-  render() {
-    return (
-        <canvas id="renderCanvas"></canvas>
-    )
-  }
-}
-
-function babylonInit() {
-  const canvas = document.getElementById('renderCanvas');
-  const engine = new Engine(canvas, true);
-  const scene = new Scene(engine);
-  const hdrTexture = new HDRCubeTexture('/public/textures/room.hdr', scene, 512);
-  const nbCase = 19;
-  const widthCaseGame = 2.5;
-  const widthWoodPlank = 57;
-
+const genPawnAppearAnimation = () => {
   const animationAppear = new Animation('appear', 'material.alpha', 50, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE);
   const keys = [{
     frame: 0,
@@ -48,112 +29,131 @@ function babylonInit() {
   }];
 
   animationAppear.setKeys(keys);
+  return animationAppear
+};
 
-  const woodPlank = MeshBuilder.CreateBox('plane', {width: 57, height: 1, depth: 57}, scene);
-  const wood = new PBRMaterial('wood', scene);
-  wood.reflectionTexture = hdrTexture;
-  wood.directIntensity = 0.5;
-  wood.environmentIntensity = 0.5;
-  wood.specularIntensity = 0.3;
-  wood.cameraExposure = 0.9;
-  wood.cameraContrast = 1.6;
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
 
-  wood.reflectivityTexture = new Texture('/public/textures/reflectivity.png', scene);
-  wood.useMicroSurfaceFromReflectivityMapAlpha = false;
-
-  wood.albedoColor = Color3.White();
-  wood.albedoTexture = new Texture('/public/textures/albedo.png', scene);
-  woodPlank.material = wood;
-
-  const marble = new PBRMaterial('marble', scene);
-  marble.reflectivityTexture = hdrTexture;
-  marble.directIntensity = 0.5;
-  marble.environmentIntensity = 0.5;
-  marble.specularIntensity = 0.3;
-  marble.cameraExposure = 0.9;
-  marble.cameraContrast = 1.6;
-  marble.alpha = 0.8;
-  marble.reflectivityTexture = new Texture('/public/textures/reflectivity.png', scene);
-
-  marble.useMicroSurfaceFromReflectivityMapAlpha = true;
-  marble.albedoColor = Color3.White();
-
-  marble.albedoTexture = new Texture('/public/textures/marble.jpg', scene);
-
-  const caseGame = MeshBuilder.CreateBox('plane', {width: 2.2, height: 1, depth: 2.2}, scene);
-  caseGame.material = marble;
-
-  caseGame.position.y = 0.3;
-  caseGame.position.x = (widthWoodPlank / 2) - (widthWoodPlank / nbCase / 2);
-  caseGame.position.z = (widthWoodPlank / 2) - (widthWoodPlank / nbCase / 2);
-  for (let i = 0; i < nbCase; i++) {
-    for (let j = 0; j < nbCase; j++) {
-      const clone = caseGame.clone('caseGame_' + j + '_' + i);
-      clone.position.x = (widthWoodPlank / 2) - (widthWoodPlank / nbCase / 2) - (widthWoodPlank / nbCase) * j;
-      clone.position.z = (widthWoodPlank / 2) - (widthWoodPlank / nbCase / 2) - (widthWoodPlank / nbCase) * i;
-    }
+    this.troll = 0;
   }
-  caseGame.dispose();
 
-  let troll = 0;
+  render() {
+    return (
+        <canvas id="renderCanvas"></canvas>
+    )
+  }
 
-  // TODO : extract
-  const addPawn = (x, y) => {
-    const pawn = new Mesh.CreateCylinder('pawn_' + x + '_' + y, 0.6, widthCaseGame * 0.8, widthCaseGame * 0.8, 0, 16, scene);
-    pawn.position.x = (widthWoodPlank / 2) - (widthWoodPlank / nbCase / 2) - (widthWoodPlank / nbCase) * x;
-    pawn.position.z = (widthWoodPlank / 2) - (widthWoodPlank / nbCase / 2) - (widthWoodPlank / nbCase) * y;
+  addPawn(x, y) {
+    const pawn = new Mesh.CreateCylinder('pawn_' + x + '_' + y, 0.6, this.widthCaseGame * 0.8, this.widthCaseGame * 0.8, 0, 16, this.scene);
+    pawn.position.x = (this.widthWoodPlank / 2) - (this.widthWoodPlank / this.nbCase / 2) - (this.widthWoodPlank / this.nbCase) * x;
+    pawn.position.z = (this.widthWoodPlank / 2) - (this.widthWoodPlank / this.nbCase / 2) - (this.widthWoodPlank / this.nbCase) * y;
     pawn.position.y = 1.1;
 
-    const wood = new PBRMaterial('wood', scene);
-    wood.reflectionTexture = hdrTexture;
+    const wood = new PBRMaterial('wood', this.scene);
+    wood.reflectionTexture = this.hdrTexture;
     wood.directIntensity = 0.5;
     wood.environmentIntensity = 0.5;
     wood.specularIntensity = 0.3;
     wood.cameraExposure = 0.9;
     wood.cameraContrast = 1.6;
 
-    wood.reflectivityTexture = new Texture('/public/textures/reflectivity.png', scene);
+    wood.reflectivityTexture = new Texture('/textures/reflectivity.png', this.scene);
     wood.useMicroSurfaceFromReflectivityMapAlpha = false;
     wood.albedoColor = Color3.White();
 
-    if (troll % 2 === 0) {
+    if (this.troll % 2 === 0) {
       wood.ambientColor = Color3.White();
     } else {
       wood.ambientColor = Color3.Red();
     }
-    troll += 1;
-    wood.albedoTexture = new Texture('/public/textures/pawn.png', scene);
+    this.troll += 1;
+    wood.albedoTexture = new Texture('/textures/pawn.png', this.scene);
     pawn.material = wood;
-    pawn.animations.push(animationAppear.clone());
-    scene.beginAnimation(pawn, 0, 60, false, 2);
-  };
+    pawn.animations.push(this.animationAppear.clone());
+    this.scene.beginAnimation(pawn, 0, 60, false, 2);
+  }
 
-  const camera = new ArcRotateCamera('Camera', -Math.PI / 4, Math.PI / 2.5, 200, Vector3.Zero(), scene);
-  camera.attachControl(canvas, true);
-  camera.minZ = 0.1;
-  camera.lowerRadiusLimit = 8;
+  componentDidMount() {
+    const canvas = document.getElementById('renderCanvas');
+    this.engine = new Engine(canvas, true);
+    this.scene = new Scene(this.engine);
+    this.hdrTexture = new HDRCubeTexture('/textures/room.hdr', this.scene, 512);
+    //this.hdrTexture = new HDRCubeTexture('/textures/room.hdr', this.scene, 64);
+    this.nbCase = config.nbCase;
+    this.widthCaseGame = 2.5;
+    this.widthWoodPlank = 57;
+    this.animationAppear = genPawnAppearAnimation();
 
-  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-  const light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene);
-  light.intensity = 0.7;
+    const woodPlank = MeshBuilder.CreateBox('plane', {width: 57, height: 1, depth: 57}, this.scene);
+    const wood = new PBRMaterial('wood', this.scene);
+    wood.reflectionTexture = this.hdrTexture;
+    wood.directIntensity = 0.5;
+    wood.environmentIntensity = 0.5;
+    wood.specularIntensity = 0.3;
+    wood.cameraExposure = 0.9;
+    wood.cameraContrast = 1.6;
+    wood.reflectivityTexture = new Texture('/textures/reflectivity.png', this.scene);
+    wood.useMicroSurfaceFromReflectivityMapAlpha = false;
+    wood.albedoColor = Color3.White();
+    wood.albedoTexture = new Texture('/textures/albedo.png', this.scene);
+    woodPlank.material = wood;
 
-  scene.onPointerDown = (evt, hitResult) => {
-    if (hitResult.hit && hitResult.pickedMesh) {
-      const string = hitResult.pickedMesh.name.split('_');
-      if (string[0] === 'caseGame') {
-        console.log('Hit case : ', parseInt(string[1], 10), '/', parseInt(string[2], 10));
-        addPawn(parseInt(string[1], 10), parseInt(string[2], 10));
+    const marble = new PBRMaterial('marble', this.scene);
+    marble.reflectivityTexture = this.hdrTexture;
+    marble.directIntensity = 0.5;
+    marble.environmentIntensity = 0.5;
+    marble.specularIntensity = 0.3;
+    marble.cameraExposure = 0.9;
+    marble.cameraContrast = 1.6;
+    marble.alpha = 0.9;
+    marble.reflectivityTexture = new Texture('/textures/reflectivity.png', this.scene);
+    marble.useMicroSurfaceFromReflectivityMapAlpha = true;
+    marble.albedoColor = Color3.White();
+    marble.albedoTexture = new Texture('/textures/marble.jpg', this.scene);
+
+    const caseGame = MeshBuilder.CreateBox('plane', {width: 2.2, height: 1, depth: 2.2}, this.scene);
+    caseGame.material = marble;
+    caseGame.position.y = 0.3;
+    caseGame.position.x = (this.widthWoodPlank / 2) - (this.widthWoodPlank / this.nbCase / 2);
+    caseGame.position.z = (this.widthWoodPlank / 2) - (this.widthWoodPlank / this.nbCase / 2);
+    for (let i = 0; i < this.nbCase; i++) {
+      for (let j = 0; j < this.nbCase; j++) {
+        const clone = caseGame.clone('caseGame_' + j + '_' + i);
+        clone.position.x = (this.widthWoodPlank / 2) - (this.widthWoodPlank / this.nbCase / 2) - (this.widthWoodPlank / this.nbCase) * j;
+        clone.position.z = (this.widthWoodPlank / 2) - (this.widthWoodPlank / this.nbCase / 2) - (this.widthWoodPlank / this.nbCase) * i;
       }
     }
-  };
+    caseGame.dispose();
 
-  engine.runRenderLoop(function () {
-    scene.render();
-  });
+    const camera = new ArcRotateCamera('Camera', -Math.PI / 4, Math.PI / 2.5, 200, Vector3.Zero(), this.scene);
+    camera.attachControl(canvas, true);
+    camera.minZ = 0.1;
+    camera.lowerRadiusLimit = 8;
 
-  window.addEventListener('resize', function () {
-    engine.resize();
-  });
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    const light = new HemisphericLight('light1', new Vector3(0, 1, 0), this.scene);
+    light.intensity = 0.7;
+
+    this.scene.onPointerDown = (evt, hitResult) => {
+      if (hitResult.hit && hitResult.pickedMesh) {
+        const string = hitResult.pickedMesh.name.split('_');
+        if (string[0] === 'caseGame') {
+          console.log('Hit case : ', parseInt(string[1], 10), '/', parseInt(string[2], 10));
+          this.addPawn(parseInt(string[1], 10), parseInt(string[2], 10));
+        }
+      }
+    };
+
+    this.engine.runRenderLoop(() => {
+      this.scene.render();
+    });
+
+    window.addEventListener('resize', () => {
+      this.engine.resize();
+    });
+  }
 }
 
 export default Game;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+  nbCase: 19
+}
