778bdcb008ad1897fde8e999ded5c116ed3b9f23
Quentin Sommer <sommerquentin67@gmail.com>
2016-11-29 11:15:18 +0100

    fix style

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import GameContainer from './GameContainer';
+import GameContainer from './GameContainer'
 
 export default class Menu extends React.Component {
   constructor(props) {
@@ -27,27 +27,27 @@ export default class Menu extends React.Component {
 
   render() {
     return (
-        <div>
-          {this.state.loadGame
-              ? <GameContainer room={this.state.room}/>
-              : this.genRoomForm()
-          }
-        </div>
+      <div>
+        {this.state.loadGame
+          ? <GameContainer room={this.state.room} />
+          : this.genRoomForm()
+        }
+      </div>
     )
   }
 
   genRoomForm = () => (
-      <div style={{
-        textAlign: 'center',
-        width: '80%',
-        margin: 'auto',
-        paddingTop: '10vh'
-      }}>
-        <h1>Choose a room to enter</h1>
-        <form onSubmit={this.handleRoomFormSubmit}>
-          <input type="number" placeholder="room number" required name="roomNumber" value={this.state.room} onChange={this.handleRoomChange}/>
-          <button style={{marginLeft: '2rem'}} type="submit">Enter room</button>
-        </form>
-      </div>
+    <div style={{
+      textAlign: 'center',
+      width: '80%',
+      margin: 'auto',
+      paddingTop: '10vh'
+    }} >
+      <h1>Choose a room to enter</h1>
+      <form onSubmit={this.handleRoomFormSubmit} >
+        <input type="number" placeholder="room number" required name="roomNumber" value={this.state.room} onChange={this.handleRoomChange} />
+        <button style={{marginLeft: '2rem'}} type="submit" >Enter room</button>
+      </form>
+    </div>
   )
 }
